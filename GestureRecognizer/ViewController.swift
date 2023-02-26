47fbef0a0462e92ca275867afc9d684f7984f11e
Harry Vologodskii <49877717+GarikVologodskiy@users.noreply.github.com>
2023-02-26 13:07:19 +0500

    initial

---
+++ GestureRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizer
+//
+//  Created by Igor Vologodskiy on 26.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
