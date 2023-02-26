3c3584f1dc15f4dbda3cfead904db1067fb68cde
Harry Vologodskii <49877717+GarikVologodskiy@users.noreply.github.com>
2023-02-26 13:31:34 +0500

    added swipeObservers and tapObservers

--- GestureRecognizer/ViewController.swift
+++ GestureRecognizer/ViewController.swift
@@ -14,6 +14,64 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        swipeObservers()
+        tapObservers()
+        
+    }
+    
+    // Observers
+    func swipeObservers() {
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+    }
+    
+    func tapObservers() {
+        
+        let tripleTap = UITapGestureRecognizer(target: self, action: #selector(tripleTapAction))
+        tripleTap.numberOfTapsRequired = 3
+        self.view.addGestureRecognizer(tripleTap)
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(doubleTapAction))
+        doubleTap.require(toFail: tripleTap)
+        doubleTap.numberOfTapsRequired = 2
+        self.view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func handleSwipes(gesture: UISwipeGestureRecognizer) {
+        
+        switch gesture.direction {
+        case .right:
+            label.text = "The right swipe was recognized"
+        case .left:
+            label.text = "The left swipe was recognized"
+        case .up:
+            label.text = "The up swipe was recognized"
+        case .down:
+            label.text = "The down swipe was recognized"
+        default:
+            break
+        }
+    }
+    
+    @objc func tripleTapAction() {
+        label.text = "Triple tap was recognized"
+    }
+    
+    @objc func doubleTapAction() {
+        label.text = "Double tap was recognized"
     }
 }
-
